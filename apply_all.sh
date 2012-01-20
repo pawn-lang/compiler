fc432e72b8f51cadf174fecc4ca9de4fc8a05486
Sergey Zolotarev <zeex@rocketmail.com>
2012-01-20 17:11:44 +0600

    Invoke "patch" with --batch

--- apply_all.sh
+++ apply_all.sh
@@ -9,9 +9,10 @@ then
 fi
 
 srcdir=$1
+patch="patch --batch"
 
-patch "$srcdir/amx/amxdbg.h"   "not_samp_compatible/amx_amxdbg.h_32bit-lines-in-header.patch"
+$patch "$srcdir/amx/amxdbg.h"   "not_samp_compatible/amx_amxdbg.h_32bit-lines-in-header.patch"
 
-patch "$srcdir/compiler/sc.h"  "samp_compatible/compiler_sc.h_increase-max-line-length.patch"
-patch "$srcdir/compiler/sc1.c" "samp_compatible/compiler_sc1.c_md-array-fix.patch"
-patch "$srcdir/compiler/sc2.c" "samp_compatible/compiler_sc2.c_stringize-ops.patch"
+$patch "$srcdir/compiler/sc.h"  "samp_compatible/compiler_sc.h_increase-max-line-length.patch"
+$patch "$srcdir/compiler/sc1.c" "samp_compatible/compiler_sc1.c_md-array-fix.patch"
+$patch "$srcdir/compiler/sc2.c" "samp_compatible/compiler_sc2.c_stringize-ops.patch"
