f9b3a18c5a52551e79ad84aa1a4c07fa0931b7c6
Sergey Zolotarev <zeex@rocketmail.com>
2012-01-20 20:04:05 +0700

    Fix file names

--- apply_all.sh
+++ apply_all.sh
@@ -11,8 +11,8 @@ fi
 srcdir=$1
 patch="patch --batch"
 
-$patch "$srcdir/amx/amxdbg.h"   "not_samp_compatible/amx_amxdbg.h_32bit-lines-in-header.patch"
+$patch "$srcdir/AMX/amxdbg.h"   "not_samp_compatible/AMX_amxdbg.h_32bit-lines-in-header.patch"
 
-$patch "$srcdir/compiler/sc.h"  "samp_compatible/compiler_sc.h_increase-max-line-length.patch"
-$patch "$srcdir/compiler/sc1.c" "samp_compatible/compiler_sc1.c_md-array-fix.patch"
-$patch "$srcdir/compiler/sc2.c" "samp_compatible/compiler_sc2.c_stringize-ops.patch"
+$patch "$srcdir/COMPILER/sc.h"  "samp_compatible/COMPILER_sc.h_increase-max-line-length.patch"
+$patch "$srcdir/COMPILER/sc1.c" "samp_compatible/COMPILER_sc1.c_md-array-fix.patch"
+$patch "$srcdir/COMPILER/sc2.c" "samp_compatible/COMPILER_sc2.c_stringize-ops.patch"
