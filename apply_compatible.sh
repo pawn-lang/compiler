29045d6411befd34e80c179a8b5bf5c30b761e6b
Zeex <zeex@rocketmail.com>
2012-06-18 22:42:32 +0600

    Add apply_compatible.sh - applies only SA-MP compatible patches

---
+++ apply_compatible.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+me=`basename $0`
+
+if [ $# -ne 1 ]
+then
+  echo "Usage: $me <source dir>"
+  exit
+fi
+
+srcdir=$1
+
+# Convert all CRLF to LF to fix patch errors on *nix
+find $srcdir -name '*.c' -or -name '*.h' | xargs sed -i 's/\r//g'
+
+# Remember the current directory
+patchdir=`pwd`
+
+# Go to the source directory and patch all files from there
+cd "$srcdir"
+
+patch="patch -p1 --force -i"
+
+$patch "$patchdir/samp_compatible/CompileTimeStringOps.patch"
+$patch "$patchdir/samp_compatible/FixMDArrayInitialization.patch"
+$patch "$patchdir/samp_compatible/IncreaseLineLimitTo4095.patch"
+$patch "$patchdir/samp_compatible/TripleStateCrash.patch"
