c8761a4ca789e1122ecc28df6dff962b4b317d45
Zeex <zeex@rocketmail.com>
2013-12-30 23:45:59 +0700

    Add a convenient shell script for Linux users
    The script simply downloads, extracts, patches and builds the code.
    
    It also partially works on Cygwin (until the build part).

---
+++ patch.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Download the source code and extract it.
+wget http://www.compuphase.com/pawn/pawn-3.2.3664.zip
+unzip pawn-3.2.3664.zip -d pawn
+
+# Apply patches.
+python patch.py --source pawn/SOURCE
+
+# Rename some directories to fix possible compile errors
+# on case-sensitive file systems.
+mv pawn/SOURCE/AMX   pawn/SOURCE/amx
+mv pawn/SOURCE/LINUX pawn/SOURCE/linux
+
+# Build the code.
+mkdir build && cd build
+cmake ../pawn/SOURCE/COMPILER
+make
