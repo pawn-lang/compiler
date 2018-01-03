pawn-lang/compiler

commit 693591bdc3491133f2aa90498c4ad8d3aadd0eb7

Don't delete trailing whitespace before '\'
new s [] = "hello \
	world";

^ needs to be "hello world", not "helloworld".

source/compiler/sc2.c
@@ -417,12 +417,9 @@ static void readline(unsigned char *line)
           ptr--;        /* skip trailing whitespace */
         if (*ptr=='\\') {
           cont=TRUE;
-          while (*(ptr-1)<=' ' && *(ptr-1)!='\0')
-            ptr--;
           /* set '\a' at the position of '\\' to make it possible to check
            * for a line continuation in a single line comment (error 49)
            */
-          /* delete trailing whitespace before continuation */
           *ptr++='\a';
           *ptr='\0';    /* erase '\n' (and any trailing whitespace) */
         } /* if */
@@ -557,7 +554,7 @@ static void stripcom(unsigned char *line)
       } /* if */
     } /* if */
     if (*line == '\a') {
-      *line = ' ';
+      memmove(line,line+1,strlen(line+1)+1);
     }
   } /* while */
   #if !defined SC_LIGHT

commit 9a49ac51b255a39c06ec539c498327f3dad7d26b

stop removing `'\a`  (internal multi-line delimiter)
This reverts a part of https://github.com/Southclaws/pawn/commit/693591bdc3491133f2aa90498c4ad8d3aadd0eb7 which caused issues as described in #226.

source/compiler/sc2.c
@@ -553,9 +553,6 @@ static void stripcom(unsigned char *line)
         } /* if */
       } /* if */
     } /* if */
-    if (*line == '\a') {
-      memmove(line,line+1,strlen((char *)line+1)+1);
-    }
   } /* while */
   #if !defined SC_LIGHT
     if (icomment==2) {