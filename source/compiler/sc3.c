dff0236743140a39a80e82ea1adfd923501214d5
Stanislav Gromov <gromovstanislav@yandex.ru>
2022-03-20 23:01:54 +0700

    Fix indentation and add a description for warning 239

--- source/compiler/sc3.c
+++ source/compiler/sc3.c
@@ -2469,7 +2469,7 @@ static int nesting=0;
               error(48);        /* array dimensions must match */
             } else {
               if (lval.sym==NULL && (arg[argidx].usage & uCONST)==0)
-                    error(239);
+                error(239);         /* literal array/string passed to a non-const parameter */
               if (arg[argidx].dim[0]!=0) {
                 assert(arg[argidx].dim[0]>0);
                 if (lval.ident==iARRAYCELL) {
