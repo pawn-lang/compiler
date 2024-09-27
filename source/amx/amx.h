3d67e9c71685d60f0948883016726f0bdf583da4
John Magdy Lotfy Kamel <19735243+Zorono@users.noreply.github.com>
2024-09-27 15:44:26 +0300

    Update amx.h

--- source/amx/amx.h
+++ source/amx/amx.h
@@ -236,7 +236,9 @@ typedef struct tagAMX_NATIVE_INFO {
 #define AMX_USERNUM     4
 #endif
 #define sEXPMAX         19      /* maximum name length for file version <= 6 */
-#define sNAMEMAX        31      /* maximum name length of symbol name */
+#ifndef sNAMEMAX
+  #define sNAMEMAX      31      /* maximum name length of symbol name */
+#endif
 
 typedef struct tagAMX_FUNCSTUB {
   ucell address         PACKED;
